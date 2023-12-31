ef8cc8bd74ddd46202dfbd55b02059828f0a1841
Shubham Phalke <onlyphalke143@gmail.com>
2023-12-31 19:46:48 +0530

    Deploying

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // frontend
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://atg-indol.vercel.app/",
         credentials: true,
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         allowedHeaders: "Content-Type,Authorization",
